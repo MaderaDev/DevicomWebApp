22ceefc9542f9d85080a9453c10ea09c6c7aa646
houssam Haned <houssam@hhmedia.fr>
2017-04-16 16:06:55 +0200

    Fix Mix elixir + menu header

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,8 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
+mix
+    .js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+//mix.copy('node_modules/bootstrap-sass/assets/fonts', 'public/css/fonts');
