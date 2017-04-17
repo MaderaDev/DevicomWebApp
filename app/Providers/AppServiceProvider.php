5d1bf41ca24830de83951f7b3cc61830eaa844cf
houssam Haned <houssam@hhmedia.fr>
2017-04-17 13:20:19 +0200

    for local env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if($this->app->environment('production')) {
+        if($this->app->environment('local')) {
             $this->app->register(Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
             $this->app->register(Way\Generators\GeneratorsServiceProvider::class);
             $this->app->register(Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
