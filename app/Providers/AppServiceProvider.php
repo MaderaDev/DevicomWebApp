eefdf5ed4c445d1f363c96f27d6338eee560e910
houssam Haned <houssam@hhmedia.fr>
2017-04-17 13:27:20 +0200

    fix local env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if($this->app->environment('local')) {
-//            $this->app->register(Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-//            $this->app->register(Way\Generators\GeneratorsServiceProvider::class);
-//            $this->app->register(Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
-//            $this->app->register(User11001\EloquentModelGenerator\EloquentModelGeneratorProvider::class);
+            $this->app->register(Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            $this->app->register(Way\Generators\GeneratorsServiceProvider::class);
+            $this->app->register(Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
+            $this->app->register(User11001\EloquentModelGenerator\EloquentModelGeneratorProvider::class);
         }
     }
 }
